0f710f8129fb48c9f2d2ec5a010a161341ff51f9
xianyao009 <yao009@hotmail.com>
2022-10-22 16:33:24 -0700

    Created HomeContainer component

--- components/containers/HomeContainer.tsx
+++ components/containers/HomeContainer.tsx
@@ -1,9 +1,27 @@
-import { Box, Container } from "@chakra-ui/react";
+import GetStartedButton from "../elements/GetStartedButton";
+import { Button, Box, Container, Heading, Stack, Text } from "@chakra-ui/react";
 
 const HomeContainer = () => {
   return (
     <>
-      <Container>This is Homecontainer</Container>
+      <Container maxW={"3xl"}>
+        <Stack align={"center"} py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "5xl" }}
+            lineHeight={"150%"}
+            textAlign={"center"}
+          >
+            Get your work done
+            <Text>
+              with ✅ Taskdone
+            </Text>
+          </Heading>
+          <Box pt={5}>
+            <GetStartedButton />
+          </Box>
+        </Stack>
+      </Container>
     </>
   );
 };

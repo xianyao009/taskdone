8be5e6c5c70dee6583b3be6408b21edf02e9d27d
xianyao009 <yao009@hotmail.com>
2022-10-21 23:20:34 -0700

    Created Logo component

---
+++ components/elements/Logo.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Button, Image, Text, Heading } from "@chakra-ui/react";
+
+const Logo = () => {
+  return (
+    <Heading as="h1">
+      <Link href="/">
+        <a>
+          <Text
+            color="black"
+            fontSize={22}
+            fontWeight="bold"
+          >
+            ✅
+            Taskdone
+          </Text>
+        </a>
+      </Link>
+    </Heading>
+  );
+};
+
+export default Logo;

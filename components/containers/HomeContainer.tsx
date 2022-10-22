9ed036082c4b8f1b2af6382ce2e9c9f825a04619
xianyao009 <yao009@hotmail.com>
2022-10-21 17:58:27 -0700

    Created HomeContainer, Layout components

---
+++ components/containers/HomeContainer.tsx
@@ -0,0 +1,13 @@
+import { Box, Container } from "@chakra-ui/react";
+
+const HomeContainer = () => {
+  return (
+    <>
+        <Container>
+            This is Homecontainer
+        </Container>
+    </>
+  )
+}
+
+export default HomeContainer;
---
+++ components/containers/Layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import Head from "next/head";
+
+type LayoutProps = {
+  children: ReactNode;
+  title?: string;
+};
+
+const Layout = ({ children, title = "Taskdone - To-do List" }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="author" content="Taskdone - To-do List" />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          sizes="16x16"
+          href="/favicon.ico"
+        />
+        <title>{title}</title>
+      </Head>
+      {children}
+    </>
+  );
+};
+
+export default Layout;

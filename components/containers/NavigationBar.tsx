3fd38036ddca200b377d88db24d5c6c1d59bc37c
xianyao009 <yao009@hotmail.com>
2022-10-22 20:08:56 -0700

    Created HamburgerMenu and added into navigation bar

--- components/containers/NavigationBar.tsx
+++ components/containers/NavigationBar.tsx
@@ -1,6 +1,8 @@
 import Logo from "../elements/Logo";
 import GetStartedButton from "../elements/GetStartedButton";
 import SignInButton from "../elements/SignInButton";
+import HamburgerMenu from "./HamburgerMenu";
+import HamburgerMenuElement from "../elements/HamburgerMenuElement";
 import ColorModeToggleButton from "../elements/ColorModeToggleButton";
 import {
   Box,
@@ -18,11 +20,17 @@ const NavigationBar = () => {
           <Logo />
         </Box>
         <HStack>
-          <SignInButton buttonVariant="ghost" />
-          <GetStartedButton />
+          <Box display={{ base: "none", md: "flex" }}>
+            <SignInButton buttonVariant="ghost" />
+            <GetStartedButton />
+          </Box>
           <Box pl={7}>
             <ColorModeToggleButton />
           </Box>
+          <HamburgerMenu>
+            {<HamburgerMenuElement linkTo="/signin" elementString="Sign In" />}
+            {<HamburgerMenuElement linkTo="/signup" elementString="Get Started" />}
+          </HamburgerMenu>
         </HStack>
       </Flex>
       <Divider borderColor={useColorModeValue("gray.300", "gray.700")} />
---
+++ components/elements/HamburgerMenuElement.tsx
@@ -0,0 +1,21 @@
+import LinkContainer from "../containers/LinkContainer";
+import {MenuItem} from "@chakra-ui/react";
+
+type HamburgerMenuElementProps = {
+  linkTo: string;
+  elementString: string;
+};
+
+const HamburgerMenuElement = ({
+  linkTo,
+  elementString,
+}: HamburgerMenuElementProps) => {
+  return (
+    <LinkContainer linkTo={linkTo}>
+        <MenuItem>{elementString}</MenuItem>
+    </LinkContainer>
+  );
+  
+};
+
+export default HamburgerMenuElement;
